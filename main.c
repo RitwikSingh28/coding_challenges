f65728ff591bcaa26fcf7a45721e2b6afbb9abce
RitwikSingh28 <itsritwik26@gmail.com>
2026-05-16 22:23:56 +0530

    feat: support byte count in ccwc

---
+++ main.c
@@ -0,0 +1,30 @@
+#include <stddef.h>
+#include <stdio.h>
+#include <string.h>
+
+int main(int argc, char** argv) {
+    if (argc < 3) {
+        fprintf(stderr, "USAGE: ccwc -<flag> <filename>\n");
+        return 1;
+    }
+    const char* flag = argv[1];
+    const char* filename = argv[2];
+
+    // open the file
+    FILE* file = fopen(filename, "r");
+    if (file == NULL) {
+        fprintf(stderr, "Could not open file: %s", filename);
+        return 1;
+    }
+    
+    if (!strcmp(flag, "-c")) {
+        // Count number of bytes in the provided file
+        size_t count = 0;
+        while (getc(file) != EOF) count++;
+
+        fprintf(stdout, " %zu\t %s\n", count, filename);
+    }
+
+    fclose(file);
+    return 0;
+}
